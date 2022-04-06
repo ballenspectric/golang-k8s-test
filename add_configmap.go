010396ea98a31911e71e6e50dc7c7545207a0bf9
Bill Allen <ballen@spectric.com>
2022-04-06 14:41:41 -0400

    Create the configmap all at once.

--- add_configmap.go
+++ add_configmap.go
@@ -5,7 +5,7 @@ import (
 	"log"
 	"path/filepath"
 
-	v1 "k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
@@ -28,12 +28,17 @@ func main() {
 		log.Fatalf(err.Error())
 	}
 
-	newConfigMap := v1.ConfigMap{
+	newConfigMap := corev1.ConfigMap{
+		//TypeMeta: metav1.TypeMeta{
+		//	Kind: "ConfigMap",
+		//	APIVersion: "v1",
+		//},
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "golang-configmap",
+			Namespace: "hello",
+		},
 		Data: map[string]string{"how": "with golang", "why": "to learn"},
 	}
-
-	newConfigMap.Name = "golang-configmap"
-	newConfigMap.Namespace = "hello"
 
 	_, err = clientset.CoreV1().ConfigMaps("hello").Create(
 		context.TODO(),
